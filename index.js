b4e328952be5d483d58f388435de9c0954938e2e
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2023-02-10 21:55:38 +0800

    add auth routes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth";
 import { register } from "./controllers/auth"
 
 // Config
@@ -39,6 +40,9 @@ const upload = multer({ storage });
 
 //Routes with files
 app.post("/auth/register", upload.single("picture"), register);
+
+//Routes
+app.use("/auth", authRoutes);
 
 //Mongoose Setup
 const PORT = process.env.PORT || 6000;
